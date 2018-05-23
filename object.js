784a5e93438e86fc793a3b09f30813eaa4b25e8d
Ancy <lazuliroseancy@gmail.com>
2018-05-23 15:07:56 +0900

    Change commentout

--- object.js
+++ object.js
@@ -64,36 +64,36 @@ console.log(count); // 100
 
 // 3. Obsever
 // Casestady : Base only once
-//function Observer() {
-//    this.listeners = [];
-//}
-//Observer.prototype.on = function(func) {
-//    this.listeners.push(func);
-//}
-//Observer.prototype.off = function(func) {
-//    var len = this.listeners.length;
-//    for (var i = 0; i < len; i++) {
-//        var listener = this.listeners[i];
-//        if (listener === func) {
-//            this.listeners.splice(i,1);
-//        }
-//    }
-//};
-//
-//Observer.prototype.trigger = function(event) {
-//    var len = this.listeners.length;
-//    for (var i = 0; i < len; i++) {
-//        var listener = this.listeners[i];
-//        listener();
-//    }
-//};
-//
-//var observer = new Observer();
-//var greet = function () {
-//    console.log("Good morning");
-//};
-//observer.on(greet);
-//observer.trigger(); // Good morning
+function Observer() {
+    this.listeners = [];
+}
+Observer.prototype.on = function(func) {
+    this.listeners.push(func);
+}
+Observer.prototype.off = function(func) {
+    var len = this.listeners.length;
+    for (var i = 0; i < len; i++) {
+        var listener = this.listeners[i];
+        if (listener === func) {
+            this.listeners.splice(i,1);
+        }
+    }
+};
+
+Observer.prototype.trigger = function(event) {
+    var len = this.listeners.length;
+    for (var i = 0; i < len; i++) {
+        var listener = this.listeners[i];
+        listener();
+    }
+};
+
+var observer = new Observer();
+var greet = function () {
+    console.log("Good morning");
+};
+observer.on(greet);
+observer.trigger(); // Good morning
 
 
 // Casestady : Base multiple
